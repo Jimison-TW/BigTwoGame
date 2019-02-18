2f560ff303eeb6093639c6b0c3c8c81877a4edba
Jimison-TW <ex3067878@yahoo.com.tw>
2019-02-18 18:23:13 +0800

    暫存變更

---
+++ Assets/Scripts/Game/Object/DropCardArea.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Object
+{
+    public class DropCardArea : MonoBehaviour
+    {
+        public void resetDropCard(List<CardComponent> cards)
+        {
+            foreach (var card in cards)
+            {
+                card.transform.SetParent(transform);
+                Vector3 endPos = transform.position;
+                Vector3 endRotation = transform.rotation.eulerAngles;
+
+                card.transform.DOMove(endPos, 1);
+                card.transform.DORotate(endRotation, 1);
+            }
+        }
+    }
+}
--- Assets/Scripts/Game/Object/PlayerComponent.cs
+++ Assets/Scripts/Game/Object/PlayerComponent.cs
@@ -11,12 +11,19 @@ namespace Assets.Scripts.Game.Object
         public ePlayerPosition position;
         private float zeroPos = -0.16f;
         private float Offset = 0.023f;
-        private List<CardComponent> chosedCardPool = new List<CardComponent>();
+        private List<CardComponent> dropCardPool = new List<CardComponent>();
         private Dictionary<int, CardComponent> handCards = new Dictionary<int, CardComponent>();
+        private UnityAction<ICardInfo> clickAction;
+
+        public void init()
+        {
+            clickAction = new UnityAction<ICardInfo>(clickCardAction);
+        }
 
         public void resetHandCard(CardComponent card)
         {
-            card.setClickCardAction(new UnityAction<ICardInfo>(clickCardAction));
+            handCards[card.cardIndex] = card;
+            card.setClickCardAction(clickAction);
             card.transform.SetParent(transform);
             Vector3 endPos = transform.position;
             Vector3 endRotation = transform.rotation.eulerAngles;
@@ -40,27 +47,27 @@ namespace Assets.Scripts.Game.Object
 
             card.transform.DOMove(endPos, 1);
             card.transform.DORotate(endRotation, 1);
+        }
 
-            handCards[card.cardIndex] = card;
+        public void setDropCardsAction()
+        {
+
         }
 
         private void clickCardAction(ICardInfo info)
         {
-            if (!info.isChoosed && position == ePlayerPosition.MySelf)
+            if (position != ePlayerPosition.MySelf) return;
+            if (!info.isChoosed&&dropCardPool.Count < 5)
             {
-                if (chosedCardPool.Count >= 5) return;
-                chosedCardPool.Add(handCards[info.cardIndex]);
+                dropCardPool.Add(handCards[info.cardIndex]);
                 handCards[info.cardIndex].isChoosed = true;
                 handCards[info.cardIndex].transform.DOMoveY(transform.position.y + 0.01f, 0.1f);
-                Debug.Log("Pool Count" + chosedCardPool.Count);
             }
             else
             {
-                if (chosedCardPool.Count <= 0) return;
-                chosedCardPool.Remove(handCards[info.cardIndex]);
+                dropCardPool.Remove(handCards[info.cardIndex]);
                 handCards[info.cardIndex].isChoosed = false;
                 handCards[info.cardIndex].transform.DOMoveY(transform.position.y, 0.1f);
-                Debug.Log("Pool Count" + chosedCardPool.Count);
             }
         }
     }
